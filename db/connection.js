5cca0b05b2c60ad46a81d05609c085ecd6649b54
Jookz <j.chatterton456@gmail.com>
2023-10-05 12:06:16 +0100

    add config to connection file

--- db/connection.js
+++ db/connection.js
@@ -9,6 +9,13 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
 console.log(process.env.PGDATABASE);
 
-module.exports = new Pool();
+module.exports = new Pool(config);
