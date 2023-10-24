b303325c760f5160e646cb29f3f4b1f8be8c0c10
Jookz <j.chatterton456@gmail.com>
2023-10-24 12:28:40 +0100

    fixed articles query

--- models/nc_news.model.js
+++ models/nc_news.model.js
@@ -95,7 +95,7 @@ exports.fetchArticles = (topic, sort_by, order) => {
   const array = [];
 
   let query = `
-    SELECT articles.article_id, title, topic, articles.author, articles.created_at, article_img_url, CAST(COUNT(comments.article_id) AS INT) AS comment_count, SUM(comments.votes) AS votes
+    SELECT articles.article_id, articles.body, articles.title, articles.topic, articles.author, articles.created_at, articles.article_img_url, articles.votes, CAST(COUNT(comments.article_id) AS INT) AS comment_count
     FROM articles
     LEFT JOIN comments 
     ON articles.article_id = comments.article_id`;
